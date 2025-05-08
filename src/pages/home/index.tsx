5350222a628d5eeded0ad040e0e073784bc1413f
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-05-07 22:42:53 -0300

    style: finish home page styled

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,20 @@
 const HomePage = () => {
   return ( 
-    <div>
-      <h1 >Pagina Home</h1>
+    <div className="flex flex-col w-full py-4  items-center justify-center">
+      <h1 className="md:text-4xl text-3xl text-white font-bold mt-10">Link Tree</h1>
+      <span className="text-white mt-3 mb-5">Veja seus Links</span>
+      <main className="flex flex-col w-11/12 max-w-xl text-center">
+        <section className="bg-white mb-4 w-full py-2 rounded-lg  select-none transition-transform hover:scale-105 cursor-pointer">
+          <a>
+            <p className="md:text-lg text-base">
+              Canal no Youtube
+            </p>
+          </a>
+        </section>
+        <footer className="flex justify-center gap-3 my-4">
+
+        </footer>
+      </main>
     </div>
    );
 }
