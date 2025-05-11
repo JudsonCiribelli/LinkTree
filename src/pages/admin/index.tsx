41ce0c76aaf12133085bc38527b885e4bd7daf02
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-05-11 11:07:34 -0300

    feat: add initial syteled on admin page and form for user add links

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,9 +1,67 @@
+import { useState } from "react";
+
 import HeaderComponent from "../../components/header-Component/header-Component";
+import InputComponent from "../../components/Input-Component/Input-Component";
 
 const AdminPage = () => {
+  
+  const [nameInput, setNameInput] = useState('')
+  const [urlInput, setUrlInput ] = useState('')
+  const [textColorInput, setTextColorInput] = useState('#f1f1f1')
+  const [backgroundColorInput, setBackgroundColorInput] = useState('#121212')
+  
   return ( 
     <div className="flex flex-col items-center min-h-screen pb-7">
       <HeaderComponent/>
+      <form className="flex flex-col my-8 w-full max-w-xl">
+         <label className="text-white font-medium my-2">Nome do link</label>
+          <InputComponent
+           placeholder="Digite o nome do seu link..."
+           value={nameInput}
+           onChange={(e) => setNameInput(e.target.value)}
+          />
+          <label className="text-white font-medium my-2">Url do link</label>
+          <InputComponent
+           type="url"
+           placeholder="Digite a url..."
+           value={urlInput}
+           onChange={(e) => setUrlInput(e.target.value)}
+          />
+
+
+          <div className="flex my-4 gap-5">
+            
+            <div>
+              <label className="text-white font-medium my-2 mx-2">Cor do link</label>
+              <input 
+               type="color" 
+               value={textColorInput} 
+               onChange={(e) => setTextColorInput(e.target.value)}
+              />
+            </div>
+
+            <div>
+              <label className="text-white font-medium my-2 mx-2">Fundo do link</label>
+              <input 
+               type="color" 
+               value={backgroundColorInput} 
+               onChange={(e) => setBackgroundColorInput(e.target.value)}
+              />
+            </div>
+          </div>
+
+            <div className="flex flex-col items-center justify-center mb-7 p-1 border-gray-100/25 border rounded">
+              <label className="text-white font-medium my-2 mx-2">Veja como esta ficando</label>
+              <article 
+               className="w-11/12 max-w-lg flex flex-col items-center justify-between bg-zinc-900 rounded px-1 py-3"
+               style={{marginBottom: 8, marginTop: 8, backgroundColor: backgroundColorInput}}
+               >
+                <p className="font-medium" style={{color: textColorInput}}>Instagram</p>
+                
+              </article>
+            </div>
+    
+      </form>
     </div>
    );
 }
