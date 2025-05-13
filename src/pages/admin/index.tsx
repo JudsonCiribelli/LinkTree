a359edbbc1cc587fc6332520b01e0380c746b95a
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-05-12 22:16:36 -0300

    style: finish initial styled on admin interface

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { FiTrash } from "react-icons/fi";
 
 import HeaderComponent from "../../components/header-Component/header-Component";
 import InputComponent from "../../components/Input-Component/Input-Component";
@@ -53,25 +54,43 @@ const AdminPage = () => {
               />
             </div>
           </div>
-
-            <div className="flex flex-col items-center justify-center mb-7 p-1 border-gray-100/25 border rounded">
-              <label className="text-white font-medium my-2 mx-2">Veja como esta ficando</label>
-              <article 
-               className="w-11/12 max-w-lg flex flex-col items-center justify-between bg-zinc-900 rounded px-1 py-3"
-               style={{marginBottom: 8, marginTop: 8, backgroundColor: backgroundColorInput}}
-               >
-                <p className="font-medium" style={{color: textColorInput}}>Instagram</p>
-                
-              </article>
-            </div>
+            
+            {nameInput !== '' && (
+              <div className="flex flex-col items-center justify-center mb-7 p-1 border-gray-100/25 border rounded">
+                <label className="text-white font-medium my-2 mx-2">Veja como esta ficando</label>
+                <article 
+                 className="w-11/12 max-w-lg flex flex-col items-center justify-between bg-zinc-900 rounded px-1 py-3"
+                 style={{marginBottom: 8, marginTop: 8, backgroundColor: backgroundColorInput}}
+                 >
+                 <p className="font-medium" style={{color: textColorInput}}>{nameInput}</p>
+                </article>
+              </div>
+            ) }
 
             <button
              type="submit" 
              className="bg-blue-400 rounded-md text-white font-medium h-9 gap-4 flex items-center justify-center mb-7 cursosr pointer"
              onClick={handleSubmitLinks}
             >Cadastrar</button>
-    
       </form>
+
+
+      <h2 className="text-white text-3xl font-medium mb-4">Meus links</h2>
+
+      <article 
+      className="flex items-center justify-between w-11/12 max-w-xl rounded py-3 px-3 mb-2 select-none"
+      style={{backgroundColor: "#5e4a4a47", color: '#ffffff'}}
+      >
+        <p>Canal do Youtube</p>
+        <div>
+          <button className="border border-dashed py-1 px-2 rounded cursor-pointer bg-black">
+            <FiTrash size={24} color="#FFF"/>
+          </button>
+        </div>
+      </article>
+    
+    
+    
     </div>
    );
 }
